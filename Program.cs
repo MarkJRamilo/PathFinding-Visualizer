de05f026b265760bf15b5547c074de998dbbde6e
MarkRamilo <lynxgamer0930@gmail.com>
2026-05-29 12:11:43 +0800

    Update modal and rename cproj

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using PathFind_Visualizer;
+using PathFinding_Visualizer;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
--- obj/Debug/net10.0/PathFind Visualizer.AssemblyInfo.cs
+++ obj/Debug/net10.0/PathFind Visualizer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PathFind Visualizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d1036a52a572e42ad120981184ddda5672891b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae00985b9d19ab13a60de7fec3265662122d0d24")]
 [assembly: System.Reflection.AssemblyProductAttribute("PathFind Visualizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PathFind Visualizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
